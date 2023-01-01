448f9f53bddac6dba3852e18f8f06b459377ea30
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2023-01-01 16:40:39 -0300

    Initial Commit

---
+++ CONCURRENCY-API_Call/CONCURRENCY_API_CallApp.swift
@@ -0,0 +1,17 @@
+//
+//  CONCURRENCY_API_CallApp.swift
+//  CONCURRENCY-API_Call
+//
+//  Created by Luiz Araujo on 01/01/23.
+//
+
+import SwiftUI
+
+@main
+struct CONCURRENCY_API_CallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
